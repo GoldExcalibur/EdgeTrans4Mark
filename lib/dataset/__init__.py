0c7e8c192bd8ad06387d9eaf54e42e22e2134acb
silvermouse <silvermouse@pku.edu.cn>
2022-07-19 13:18:45 +0800

    dataset init mod

--- lib/dataset/__init__.py
+++ lib/dataset/__init__.py
@@ -3,5 +3,3 @@ from __future__ import division
 from __future__ import print_function
 
 from .cepha import CEPHADataset as cepha
-from .hand import HANDDataset as hand
-from .chest import CHESTDataset as chest
